08aaafe24639d88469c2e7d5ac4a5913fc3ee97e
Idan Ben Shalom <idanbensha@gmail.com>
2022-10-25 23:10:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='google-trans-new',
+setup(name='google_trans_new',
       version='1.2.0',
       description='Google translate new',
       author='...',
