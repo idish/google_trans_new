dc9c20c41e633a114966f95804861a43149cf4cc
Idan Ben Shalom <idanbensha@gmail.com>
2022-10-25 23:17:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='google_trans_new',
+setup(name='google-trans-new',
       version='1.2.1',
       description='Google translate new',
       author='...',
       author_email='gward@python.net',
       url='https://www.python.org/sigs/distutils-sig/',
-      packages=[],
+      packages=['google_trans_new'],
      )
