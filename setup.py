0485047da3cd2136fad62e3d37b71a9ece73529d
Idan Ben Shalom <idanbensha@gmail.com>
2022-10-25 23:01:55 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='google-trans-new',
+      version='1.2.0',
+      description='Google translate new',
+      author='...',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=[],
+     )
